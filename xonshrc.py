2f6e394e4954afddb304e03c5fe4bfbb1e4cc2de
Cassandra <cassandra+git@services.beelen.one>
2020-06-05 16:56:02 +0200

    Update xonsh colors

--- xonshrc.py
+++ xonshrc.py
@@ -8,6 +8,6 @@ aliases['dc'] = ['docker-compose']
 aliases['la'] = ['ls', '-hAl']
 
 # XONSH WEBCONFIG START
-$PROMPT = '{env_name}{BOLD_GREEN}{user}@{hostname}{BOLD_BLUE} {short_cwd}{branch_color}{gitstatus: {}}{NO_COLOR} {BOLD_BLUE}{prompt_end}{NO_COLOR} '
+$PROMPT = '{env_name}{BOLD_CYAN}{user}{NO_COLOR}@{BOLD_YELLOW}{hostname}{BOLD_BLUE} {short_cwd}{branch_color}{gitstatus: {}}{NO_COLOR} {BOLD_BLUE}{prompt_end}{NO_COLOR} '
 $XONSH_COLOR_STYLE = 'monokai'
 # XONSH WEBCONFIG END
