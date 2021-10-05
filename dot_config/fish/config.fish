d05cb96e6e4791d554f8b6037e1cbd6f5b576fc6
Cassandra Beelen <cassandra.beelen@rewe-digital.com>
2021-10-05 10:48:32 +0200

    feat: limit task output

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -15,7 +15,7 @@ end
 function fish_greeting
     if not status --is-login; and status --is-interactive
         if type -q task; and test -d ~/.task/
-            task next
+            task next limit:page
         end
     end
 end
