2ef93483fb9fbf986879473822ce0a3c62b97161
Cassandra <keen.flag3479@phoenix.qcx.io>
2021-12-25 16:19:37 +0100

    feat(fish): rm to trash, bashrc

--- dot_config/fish/conf.d/aliases.fish
+++ dot_config/fish/conf.d/aliases.fish
@@ -7,6 +7,12 @@ if type -q doas
     abbr --add sudo doas
 end
 
+# rm -> trash
+if type -q trash
+    abbr --add rm trash
+end
+abbr --add rma rm
+
 # kubernetes
 if not type -q kustomize; and type -q kubectl
     abbr --add kustomize "kubectl kustomize"
