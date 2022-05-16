ac76975ff255c35334a818969613ce715166fd4d
Cassandra <keen.flag3479@phoenix.qcx.io>
2022-05-16 08:47:13 +0200

    feat(fish): more aliases

--- dot_config/fish/conf.d/aliases.fish
+++ dot_config/fish/conf.d/aliases.fish
@@ -1,31 +1,40 @@
 # general utility
-abbr --add userctl "systemctl --user"
+if type -q systemctl
+    abbr -g -a userctl "systemctl --user"
+end
 
 if not type -q docker; and type -q podman
-    abbr --add docker podman
+    abbr -g -a docker podman
 end
 
 # elevated commands
 if type -q doas
-    abbr --add sudo doas
-    abbr --add systemctl "doas systemctl"
+    abbr -g -a sudo doas
+    abbr -g -a systemctl "doas systemctl"
 else
-    abbr --add systemctl "sudo systemctl"
+    abbr -g -a systemctl "sudo systemctl"
 end
 
 # rm -> trash
 if type -q trash
-    abbr --add rm trash
+    abbr -g -a rm trash
 end
-abbr --add rma rm
+abbr -g -a rma rm
 
 # kubernetes
 if not type -q kustomize; and type -q kubectl
-    abbr --add kustomize "kubectl kustomize"
+    abbr -g -a kustomize "kubectl kustomize"
 end
 
 # git
 if type -q git
-    abbr --add add "git add"
-    abbr --add commit "git commit"
+    function _git_primary_branch
+        git symbolic-ref refs/remotes/origin/HEAD | string split -r -m1 -f2 '/'
+    end
+
+    abbr -g -a ga "git add"
+    abbr -g -a gc "git commit"
+    abbr -g -a gch "git checkout"
+    abbr -g -a gcm 'git checkout $(_git_primary_branch); and git pull'
+    abbr -g -a gmm 'git fetch; and git merge origin/$(_git_primary_branch)'
 end
