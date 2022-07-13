5fbca7fab8c5bb5db2ae854f00500df672d7dce0
Cassandra <keen.flag3479@phoenix.qcx.io>
2022-07-13 10:49:40 +0200

    feat(fish): linuxbrew support

--- dot_config/fish/conf.d/environment.fish
+++ dot_config/fish/conf.d/environment.fish
@@ -18,26 +18,24 @@ end
 # macOS python
 if [ -d $HOME/Library/Python ]
     for path in $HOME/Library/Python/*/bin
-        fish_add_path -m $path
+        fish_add_path -g -m $path
     end
 end
 
 # brew
-if [ -d /opt/homebrew ]
-    set -gx HOMEBREW_PREFIX /opt/homebrew
-    set -gx HOMEBREW_CELLAR $HOMEBREW_PREFIX/Cellar
-    set -gx HOMEBREW_REPOSITORY $HOMEBREW_PREFIX
-    fish_add_path -m $HOMEBREW_PREFIX/bin
-    fish_add_path -m $HOMEBREW_PREFIX/sbin
+if string match -q -e Darwin (uname); and path is -d /opt/homebrew
+    eval (/opt/homebrew/bin/brew shellenv)
+else if string match -d -e Linux (uname); and path is -d /home/linuxbrew/.linuxbrew
+    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 end
 
 # local binaries
 if [ -d $GOPATH/bin ]
-    fish_add_path -m $GOPATH/bin
+    fish_add_path -g -m $GOPATH/bin
 end
 if [ -d $HOME/.local/bin ]
-    fish_add_path -m $HOME/.local/bin
+    fish_add_path -g -m $HOME/.local/bin
 end
 if [ -d $HOME/dev/scripts ]
-    fish_add_path -m $HOME/dev/scripts
+    fish_add_path -g -m $HOME/dev/scripts
 end
