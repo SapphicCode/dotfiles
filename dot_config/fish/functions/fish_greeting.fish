c68b2fad723577df08ffb0e09d15648347a649f1
Cassandra <6bb29412-bf37-4003-a415-9f1edbc07939@sapphicco.de>
2022-10-03 12:49:15 +0200

    fix(fish.greeting): overcomplications

--- dot_config/fish/functions/fish_greeting.fish
+++ dot_config/fish/functions/fish_greeting.fish
@@ -1,11 +1,5 @@
 function fish_greeting
     if status --is-interactive; and [ "$TERM_PROGRAM" != "vscode" ]; and [ -z "$SSH_CLIENT" ]
-        if type -q task; and test -d ~/.task/
-            if type -q timew; and timew :quiet
-                task next limit:page -major
-            else
-                task next limit:page project.not:Work -major
-            end
-        end
+        task next limit:page
     end
 end
