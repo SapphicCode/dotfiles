5616edfa6a6ee909202df9eb09ab76167f5e347f
Cassandra <git@sapphicco.de>
2022-10-19 14:18:47 +0200

    fix(fish): fail greeting gracefully

--- dot_config/fish/functions/fish_greeting.fish
+++ dot_config/fish/functions/fish_greeting.fish
@@ -1,5 +1,7 @@
 function fish_greeting
     if status --is-interactive; and [ "$TERM_PROGRAM" != "vscode" ]; and [ -z "$SSH_CLIENT" ]
-        task next limit:page
+        if type -q task
+            task next limit:page
+        end
     end
 end
