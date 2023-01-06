03b0eafcece48e04e06c7207b6f4c5d80d3f704f
Cassandra <git@sapphicco.de>
2023-01-06 21:44:01 +0100

    feat(fish): set up pnpm home
    * fix(fish): move brew up
    * fix(fish): only set go environment if go is present

--- dot_config/fish/conf.d/environment.fish
+++ dot_config/fish/conf.d/environment.fish
@@ -1,8 +1,17 @@
 set -l platform (uname | string lower)
 
+# brew
+if string match -q -e $platform darwin; and path is -d /opt/homebrew
+    eval (/opt/homebrew/bin/brew shellenv)
+else if string match -q -e $platform linux; and path is -d /home/linuxbrew/.linuxbrew
+    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+end
+
 # Go
-set -x GOPATH $HOME/dev/go
-set -x CGO_ENABLED 0
+if type -q go
+    set -x GOPATH $HOME/dev/go
+    set -x CGO_ENABLED 0
+end
 
 # Editors
 if type -q nvim
@@ -17,11 +26,11 @@ if type -q gcloud; and type -q python3.9
     set -x CLOUDSDK_PYTHON (type --path python3.9)
 end
 
-# brew
-if string match -q -e $platform darwin; and path is -d /opt/homebrew
-    eval (/opt/homebrew/bin/brew shellenv)
-else if string match -q -e $platform linux; and path is -d /home/linuxbrew/.linuxbrew
-    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+# pnpm
+if type -q pnpm
+    set -x PNPM_HOME $HOME/.local/share/pnpm
+    mkdir -p $PNPM_HOME
+    fish_add_path -g -m $PNPM_HOME
 end
 
 # local binaries
