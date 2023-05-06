633c818f317beac28be12727f8799f0da78d2329
Cassandra <git@sapphicco.de>
2023-05-06 13:52:36 +0200

    fish: update taskwarrior integration

--- dot_config/fish/functions/fish_greeting.fish
+++ dot_config/fish/functions/fish_greeting.fish
@@ -1,7 +1,11 @@
 function fish_greeting
     if status --is-interactive; and [ "$TERM_PROGRAM" != "vscode" ]; and [ -z "$SSH_CLIENT" ]
+        # Taskwarrior integration
         if type -q task
-            task next limit:page
+            set -l count (task +READY count 2> /dev/null)
+            if [ $count -gt 0 ]
+                echo $count "tasks ready."
+            end
         end
     end
 end
