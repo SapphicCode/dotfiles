368bdde20059d350de703d84b15c27e6d250c1e9
Cassandra <git@sapphicco.de>
2023-11-22 18:05:21 +0100

    fish: handle weirder app names

--- dot_config/fish/platform/darwin.fish
+++ dot_config/fish/platform/darwin.fish
@@ -1,7 +1,7 @@
 # CLI application launchers
 if type -q fd
     for app in (fd --max-depth 2 -t directory '\.app$' /Applications ~/Applications)
-        set -l app_safe (string replace -a " " "" (basename $app))
+        set -l app_safe (string replace -r -a "[^\w.]" "" (basename $app))
         alias $app_safe "open -a '$app'"
     end
 end
