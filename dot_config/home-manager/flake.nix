b5c37ab80e3f85001b5417e7936c0c80f06153ef
Cassandra <git@sapphicco.de>
2023-12-04 22:18:41 +0100

    home-manager: try host config with flake

--- dot_config/home-manager/flake.nix
+++ dot_config/home-manager/flake.nix
@@ -11,20 +11,14 @@
   };
 
   outputs = { nixpkgs, home-manager, ... }:
-    let
-      system = "aarch64-darwin";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
     {
-      homeConfigurations."sapphiccode" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ./home.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
+      homeConfigurations."sapphiccode@Maeve" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages."aarch64-darwin";
+        modules = [ ./host/Maeve.nix ];
+      };
+      homeConfigurations."deck@hollydeck" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages."x86_64-linux";
+        modules = [ ./home/hollydeck.nix ];
       };
     };
 }
