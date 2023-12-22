23cd47350b0fd8c3a7f1ab6b2111d45c3296cb27
Cassandra <git@sapphicco.de>
2023-12-22 20:24:38 +0100

    home-manager: unfuck x86_64-linux (home-manager release-23.11 is just broken apparently)

--- dot_config/home-manager/flake.nix
+++ dot_config/home-manager/flake.nix
@@ -3,30 +3,31 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
-    {
-      homeConfigurations."sapphiccode@Maeve" = home-manager.lib.homeManagerConfiguration {
+  outputs = { nixpkgs, home-manager, ... }: {
+    homeConfigurations = {
+      "sapphiccode@Maeve" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages."aarch64-darwin";
         modules = [ ./host/Maeve.nix ];
       };
-      homeConfigurations."deck@hollydeck" = home-manager.lib.homeManagerConfiguration {
+      "deck@hollydeck" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages."x86_64-linux";
         modules = [ ./host/hollydeck.nix ];
       };
-      homeConfigurations."generic-server-arm64" = home-manager.lib.homeManagerConfiguration {
+      "generic-server-arm64" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages."aarch64-linux";
         modules = [ ./host/generic-server.nix ];
       };
-      homeConfigurations."generic-server-amd64" = home-manager.lib.homeMangerConfiguration {
+      "generic-server-amd64" = home-manager.lib.homeMangerConfiguration {
         pkgs = nixpkgs.legacyPackages."x86_64-linux";
         modules = [ ./host/generic-server.nix ];
       };
     };
+  };
 }
