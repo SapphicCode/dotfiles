b60eb194a0a92341f9df16843a71a6114bfc14e8
Cassandra <git@sapphicco.de>
2023-12-30 20:24:59 +0100

    home-manager: move configuration elsewhere

--- dot_config/home-manager/host/generic-server.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }: {
-  home.username = "sapphiccode";
-  home.homeDirectory = "/home/sapphiccode";
-  home.stateVersion = "23.05";
-
-  imports = [
-    ../profile/comfortable.nix
-  ];
-}
