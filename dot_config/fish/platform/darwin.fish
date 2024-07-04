c1f1ecc0a426daea791154afc1ff0a61361b7154
Cassandra <git@sapphicco.de>
2024-07-04 09:54:28 +0200

    ssh: completely rework agent handling

--- dot_config/fish/platform/darwin.fish
+++ dot_config/fish/platform/darwin.fish
@@ -3,6 +3,16 @@ if string match -q "/private/tmp/com.apple.launchd*/Listeners" $SSH_AUTH_SOCK
     set -e -g SSH_AUTH_SOCK
 end
 
+# SSH agents: Secretive for regular SSH, 1Password for Git
+if [ -z $SSH_CLIENT ] # check we're not remote
+    set -x SSH_AUTH_SOCK $HOME/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
+    function git --wraps git
+        set -f -x SSH_AUTH_SOCK $HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock
+        command git $argv
+        return $status
+    end
+end
+
 # CLI application launchers
 if type -q fd
     for app in (fd --max-depth 2 -t directory '\.app$' /Applications ~/Applications)
