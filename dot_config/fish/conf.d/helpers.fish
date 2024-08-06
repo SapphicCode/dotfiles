8d3e5368c0473ac7f77c48fc69540e6831d9a31b
Cassandra <git@sapphicco.de>
2024-08-06 21:32:24 +0200

    fish: disable atuin on ZFS, typo fix

--- dot_config/fish/conf.d/helpers.fish
+++ dot_config/fish/conf.d/helpers.fish
@@ -7,7 +7,7 @@ end
 if type -q atuin; and not begin
         # atuin and ZFS (more specifically, SQLite) do not play nice, disable it
         # https://github.com/atuinsh/atuin/issues/952
-        type -q findmnt; and findmnt --target $HOME | grep -q zfs; and type -q zfs; and [ (zfs get -Ho value sync $HOME) = "disabled" ]
+        type -q findmnt; and findmnt --target $HOME | grep -q zfs; and type -q zfs; and [ (zfs get -Ho value sync $HOME) != "disabled" ]
     end
 
     atuin init fish | source
