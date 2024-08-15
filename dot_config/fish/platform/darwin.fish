a1405bd2f97f681b267841c6a1eb5e45c9a79ad5
Cassandra <git@sapphicco.de>
2024-08-15 17:56:59 +0200

    git: introduce wrapper script

--- dot_config/fish/platform/darwin.fish
+++ dot_config/fish/platform/darwin.fish
@@ -3,16 +3,6 @@ if string match -q "/private/tmp/com.apple.launchd*/Listeners" $SSH_AUTH_SOCK
     set -e -g SSH_AUTH_SOCK
 end
 
-# SSH agents: Secretive for regular SSH, 1Password for Git
-if [ -z $SSH_CLIENT ] # check we're not remote
-    set -x SSH_AUTH_SOCK $HOME/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
-    function git --wraps git
-        set -f -x SSH_AUTH_SOCK $HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock
-        command git $argv
-        return $status
-    end
-end
-
 # iTerm shell integration
 if status is-interactive; and [ $TERM_PROGRAM = "iTerm.app" ]
     source ~/.config/fish/iterm_integration.fish
