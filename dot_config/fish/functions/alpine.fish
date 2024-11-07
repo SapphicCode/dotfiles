0714834dd00f5cb8df7a39d260b7ebc6ef3078b6
Cassandra <git@sapphicco.de>
2024-11-07 19:46:05 +0100

    fix(fish): alpine function

--- dot_config/fish/functions/alpine.fish
+++ dot_config/fish/functions/alpine.fish
@@ -1,4 +1,7 @@
-function alpine --wraps='podman run --rm -it docker.io/library/alpine:3.17' --description 'alias alpine podman run --rm -it docker.io/library/alpine:3.17'
-  podman run --rm -it docker.io/library/alpine:3.17 $argv
-        
+function alpine --wraps='docker run --rm -it quay.io/sapphiccode/alpine-cicd:latest'
+    set -f runtime podman
+    if type -q docker && [ (docker context show 2> /dev/null) = orbstack ]
+        set -f runtime docker
+    end
+    $runtime run --rm -it $argv quay.io/sapphiccode/alpine-cicd:latest
 end
