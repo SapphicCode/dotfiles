dfb2d5318c314384c302757d3ca126c860b6d400
Cassandra <git@sapphicco.de>
2025-01-17 22:32:49 +0100

    fix(fish): avoid zellij inside vscode remote

--- dot_config/fish/conf.d/environment.fish
+++ dot_config/fish/conf.d/environment.fish
@@ -6,7 +6,7 @@ if not begin
 end
 
 # auto-attach zellij in SSH if session present
-if test "$SSH_CONNECTION"; and test -z "$ZELLIJ"; and status is-interactive; and type -q zellij
+if test "$SSH_CONNECTION"; and status is-interactive; and test -z "$ZELLIJ"; and test "$TERM_PROGRAM" != "vscode"; and type -q zellij
     set -l start (date '+%s')
     if zellij list-sessions --no-formatting | grep --invert-match EXITED | grep -q ssh
         zellij attach ssh
