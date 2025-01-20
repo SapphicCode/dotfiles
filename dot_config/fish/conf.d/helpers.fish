54b7099e9ac3941dd92d2970c2873fcd4e5c6f16
Cassandra <git@sapphicco.de>
2025-01-20 12:04:35 +0100

    feat(fish): add hook for devbox global

--- dot_config/fish/conf.d/helpers.fish
+++ dot_config/fish/conf.d/helpers.fish
@@ -7,8 +7,11 @@ end
 if type -q atuin; and not begin
         # atuin and ZFS (more specifically, SQLite) do not play nice, disable it
         # https://github.com/atuinsh/atuin/issues/952
-        type -q findmnt; and findmnt --target $HOME | grep -q zfs; and type -q zfs; and [ (zfs get -Ho value sync $HOME) != "disabled" ]
+        type -q findmnt; and findmnt --target $HOME | grep -q zfs; and type -q zfs; and [ (zfs get -Ho value sync $HOME) != disabled ]
     end
 
     atuin init fish | source
+end
+if type -q devbox
+    devbox global shellenv -r | source
 end
