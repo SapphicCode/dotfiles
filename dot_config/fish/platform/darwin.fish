dbbbf3dd0bbf03cba6177786be493b56d66089db
Cassandra <git@sapphicco.de>
2025-05-19 08:58:34 +0200

    fix(fish): syntax

--- dot_config/fish/platform/darwin.fish
+++ dot_config/fish/platform/darwin.fish
@@ -4,8 +4,8 @@ if string match -q "/private/tmp/com.apple.launchd*/Listeners" "$SSH_AUTH_SOCK"
 end
 
 # iTerm shell integration
-if status is-interactive; and [ "$TERM_PROGRAM" = "iTerm.app" ] and test -f ~/.config/fish/iterm_integration.fish
-    source ~/.config/fish/iterm_integration.fish
+if status is-interactive; and [ "$TERM_PROGRAM" = "iTerm.app" ] and [ -f "$HOME/.config/fish/iterm_integration.fish" ]
+    source "$HOME/.config/fish/iterm_integration.fish"
 end
 
 # CLI application launchers
