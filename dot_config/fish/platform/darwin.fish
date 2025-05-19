a719997f8670ce40c50df302a48f9e499ab6c966
Cassandra <git@sapphicco.de>
2025-05-19 08:57:44 +0200

    fix(fish): only load iterm2 shell integration when available

--- dot_config/fish/platform/darwin.fish
+++ dot_config/fish/platform/darwin.fish
@@ -4,7 +4,7 @@ if string match -q "/private/tmp/com.apple.launchd*/Listeners" "$SSH_AUTH_SOCK"
 end
 
 # iTerm shell integration
-if status is-interactive; and [ "$TERM_PROGRAM" = "iTerm.app" ]
+if status is-interactive; and [ "$TERM_PROGRAM" = "iTerm.app" ] and test -f ~/.config/fish/iterm_integration.fish
     source ~/.config/fish/iterm_integration.fish
 end
 
