f88a4c08d84e6b309ce139d91b25cd2948c41a07
Cassandra <git@sapphicco.de>
2026-04-05 14:31:08 +0200

    fix(fish/jj): run in a fresh shell once

--- dot_config/fish/conf.d/prompt.fish
+++ dot_config/fish/conf.d/prompt.fish
@@ -2,12 +2,14 @@ if status is-interactive
     if type -q starship
         eval (starship init fish)
     end
-end
 
-function _jj_check --on-variable PWD
-    if type -q jj; and jj root --ignore-working-copy &>/dev/null
-        set -gx STARSHIP_CONFIG "$HOME/.config/starship-jj.toml"
-    else
-        set -e STARSHIP_CONFIG
+    function _jj_check --on-variable PWD
+        if type -q jj; and jj root --ignore-working-copy &>/dev/null
+            set -gx STARSHIP_CONFIG "$HOME/.config/starship-jj.toml"
+        else
+            set -e STARSHIP_CONFIG
+        end
     end
+
+    _jj_check
 end
