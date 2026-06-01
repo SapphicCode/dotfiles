7cf8b1dfef7fa970c5c3024eb2b0a324b6f8e31f
Cassandra <git@sapphicco.de>
2026-06-01 14:05:09 +0200

    feat(hammerspoon): auto-arrange some windows

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -1,5 +1,4 @@
 -- Auto-close specific applications if they have zero windows and lose focus
-
 local function table_contains(tbl, element)
   for _, value in ipairs(tbl) do
     if value == element then
@@ -27,3 +26,77 @@ hs.application.watcher.new(function(name, event, app)
     closeIfZero(name)
   end
 end):start()
+
+-- Automatically align some windows when focused
+local windowRatios = {
+  ["Helium"] = "twoThirds",
+  ["Zen"] = "twoThirds",
+  ["Code"] = "fullscreen",
+  ["Microsoft Teams"] = "fullscreen",
+}
+
+local screenRatios = {
+  ["LG ULTRAGEAR (2)"] = "twoThirdsRight",
+  ["LG ULTRAGEAR (1)"] = "twoThirdsLeft",
+}
+
+local function alignWindow(win)
+  if not win:isStandard() then
+    return -- not a standard window, do nothing
+  end
+
+  local screen = win:screen()
+  local screenFrame = screen:frame()
+  local screenFrameUsable = hs.geometry.rect(screenFrame.x + 12, screenFrame.y + 12, screenFrame.w - 24,
+    screenFrame.h - 24)
+
+  local twoThirdsLeft = hs.geometry.rect(screenFrameUsable.x, screenFrameUsable.y, screenFrameUsable.w * 2 / 3,
+    screenFrameUsable.h)
+  local twoThirdsRight = hs.geometry.rect(screenFrameUsable.x + screenFrameUsable.w / 3, screenFrameUsable.y,
+    screenFrameUsable.w * 2 / 3,
+    screenFrameUsable.h)
+
+  local targetRatio = windowRatios[win:application():name()]
+  if not targetRatio then
+    return -- no match for this app, do nothing
+  end
+
+  local targetRatioByScreen = screenRatios[screen:name()]
+  if targetRatio == "fullscreen" then
+    win:setFrame(screenFrameUsable)
+  elseif targetRatio == "twoThirds" and targetRatioByScreen == "twoThirdsLeft" then
+    win:setFrame(twoThirdsLeft)
+  elseif targetRatio == "twoThirds" and targetRatioByScreen == "twoThirdsRight" then
+    win:setFrame(twoThirdsRight)
+  end
+end
+
+-- Automatically assign some windows to specific spaces based on window title
+local spaceAssignments = {
+  ["~/dev/work/"] = { 3, 4 },
+  ["simone"] = { 3, 4 },
+}
+
+local function assignSpace(win)
+  local title = win:title()
+
+  -- check if window not already in allowed space
+  local currentSpace = win:space()
+
+  for pattern, spaces in pairs(spaceAssignments) do
+    if string.match(title, pattern) then
+      local space = spaces[1] -- For simplicity, just use the first space in the list
+      hs.spaces.moveWindowToSpace(win:id(), space)
+      break
+    end
+  end
+end
+
+-- Subscribe event handlers
+local windowFilter = hs.window.filter.new()
+windowFilter:subscribe(hs.window.filter.windowCreated, function(win)
+  alignWindow(win)
+end)
+windowFilter:subscribe(hs.window.filter.windowFocused, function(win)
+  alignWindow(win)
+end)
